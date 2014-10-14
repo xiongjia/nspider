9408b1d8b1c89a854224cae9cd3685ba3c04a523
xiongjia <lexiongjia@gmail.com>
2014-10-14 21:00:28 +0800

    update http hdr -> user-agent

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 'use strict';
 
+var misc = require('./lib/misc.js'),
+  dbgTrace = misc.dbgTrace('nspider');
+
 exports = module.exports = function (opts) {
-  var misc;
+  var defOpts;
 
   /* update logger handler */
   opts = opts || {};
-  misc = require('./lib/misc.js');
   misc.logger.update({ silent: opts.silent, handler: opts.logger });
+
+  /* update default options */
+  defOpts = misc.defaultOpts;
+  defOpts.proxy = opts.proxy || defOpts.proxy;
+  defOpts.fetchTimeout = opts.fetchTimeout || defOpts.fetchTimeout;
+  defOpts.httpHdrUsrAgent = opts.httpHdrUsrAgent || defOpts.httpHdrUsrAgent;
+  dbgTrace('%s options: %j', misc.nspider.getVer(), defOpts);
   return {
-    Spider: require('./lib/spider.js')
+    Spider: require('./lib/spider.js'),
+    version: {
+      majorVer: misc.nspider.getMajorVer(),
+      minorVer: misc.nspider.getMinorVer(),
+      ver: misc.nspider.getVer()
+    }
   };
 };
 
