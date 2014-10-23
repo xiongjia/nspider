b1a1f7a2aa8125a085143f28d4abd3f36c6c28e2
xiongjia <lexiongjia@gmail.com>
2014-10-23 19:42:02 +0800

    update example

--- examples/list_all_links.js
+++ examples/list_all_links.js
@@ -6,11 +6,20 @@ var nspider = require('../')();
   var spider;
 
   /* create spider object */
-  spider = nspider.Spider();
-  spider.on('fetchCompleted', function (data) {
+  spider = new nspider.Spider();
+
+  /* fetch the content of 'http://nodejs.org' */
+  spider.fetch('http://nodejs.org', function (err, data) {
+    if (err) {
+      /* fetch error */
+      console.log('fetchError: %s', err.toString());
+      return;
+    }
+
     if (data.contentType.type !== 'text' ||
         data.contentType.subtype !== 'html') {
       /* Not a HTML page */
+      console.log('Invalid contentType: %j', data.contentType);
       return;
     }
 
@@ -18,12 +27,6 @@ var nspider = require('../')();
     data.$('a').each(function (idx,  item) {
       console.log('Link[%d]: %s', idx, data.$(item).attr('href'));
     });
-  }).on('fetchError', function (err) {
-    /* fetch error */
-    console.log('fetchError: %s', err.toString());
   });
-  
-  /* check the 'http://nodejs.org' */ 
-  spider.fetch('http://nodejs.org');
 })();
 
